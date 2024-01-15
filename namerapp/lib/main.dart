0bf85d90e9b0c3b756032070758848a54749c084
elkibet <ktonneh@gmail.com>
2024-01-15 11:38:08 +0300

    initial commit

---
+++ namerapp/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+    // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+     var pair = appState.current;  
+     var headerText = 'RANDOM GREAT IDEAS'; 
+     var nextButtonText = 'Next';
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(headerText),
+          BigCard(pair: pair),
+          // ↓ Add Button .
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+              appState.getNext();
+            },
+            child: Text(nextButtonText),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
+  }
+}
