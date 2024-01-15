3f4f5bc4d17376b2c28c08772cf248c6a1cc44fe
elkibet <ktonneh@gmail.com>
2024-01-15 13:20:07 +0300

    add favorites button

--- namerapp/lib/main.dart
+++ namerapp/lib/main.dart
@@ -33,6 +33,18 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite(){
+    if(favorites.contains(current)){
+      favorites.remove(current);
+    } else{
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+
 }
 
 class MyHomePage extends StatelessWidget {
@@ -42,21 +54,44 @@ class MyHomePage extends StatelessWidget {
      var pair = appState.current;  
      var headerText = 'RANDOM GREAT IDEAS'; 
      var nextButtonText = 'Next';
+    
+    IconData icon;
+    if(appState.favorites.contains(pair)){
+      icon = Icons.favorite;
+    } else{
+      icon  = Icons.favorite_border;
+    }
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text(headerText),
-          BigCard(pair: pair),
-          // ↓ Add Button .
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-              appState.getNext();
-            },
-            child: Text(nextButtonText),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(headerText),
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            // ↓ Add Button .
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: (){
+                  appState.toggleFavorite();
+                }, 
+                icon: Icon(icon), 
+                label: Text('like')),
+                SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: () {
+                print('button pressed!');
+                appState.getNext();
+              },
+              child: Text(nextButtonText),
+            )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -81,7 +116,10 @@ class BigCard extends StatelessWidget {
       elevation: 20,
       color: theme.colorScheme.primary,
       child: Padding(padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase,style: style)
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",) //Ideal for Accessibility by screen readers
       ),
     );
 
